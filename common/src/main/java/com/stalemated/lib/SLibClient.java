9d1e4e92e4be3f608f3b3619bec6489f4d0ecead
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-06-15 13:08:15 -0600

    Fix: fixed mod id (changed to s_lib) and made YACL a compile only dependency (not required by clients, only needed to import code)

--- common/src/main/java/com/stalemated/lib/SLibClient.java
+++ common/src/main/java/com/stalemated/lib/SLibClient.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class SLibClient {
-    public static final String MOD_ID = "stalelib";
+    public static final String MOD_ID = "s_lib";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
-        LOGGER.info("Stale Lib loaded successfully");
+        LOGGER.info("S-Lib loaded successfully");
     }
 }
