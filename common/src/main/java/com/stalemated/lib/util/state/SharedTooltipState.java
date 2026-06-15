2b54686ec5ca789e50f8b3bad1739de40ae83d34
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-06-15 15:50:31 -0600

    Feat: added a tooltip state manager that stores a global value to determine if a tooltip needs to have scrolling enabled or not if STS is installed

---
+++ common/src/main/java/com/stalemated/lib/util/state/SharedTooltipState.java
@@ -0,0 +1,7 @@
+package com.stalemated.lib.util.state;
+
+public class SharedTooltipState {
+    /*If installed, STS will force a tooltip to abide by
+    the custom dimensions rules when another mod modifies this value*/
+    public static boolean forceCustomDimensions = false;
+}
