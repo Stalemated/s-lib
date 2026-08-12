99774e339d7f95ab3bda17e0e4f96d53b640a2e5
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-08-12 01:25:02 -0600

    Feat: add an easy way to register keybinds for both forge and fabric in PlatformHelper, including an accessor and a method to check if a key is being actively pressed

--- forge/src/main/java/com/stalemated/lib/forge/ForgePlatformHelper.java
+++ forge/src/main/java/com/stalemated/lib/forge/ForgePlatformHelper.java
@@ -4,10 +4,15 @@ import com.stalemated.lib.helper.PlatformHelper;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.fml.loading.LoadingModList;
+import net.minecraft.client.option.KeyBinding;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ForgePlatformHelper implements PlatformHelper {
+    public static final List<KeyBinding> KEYBINDINGS = new ArrayList<>();
+
     @Override
     public Path getConfigDir() {
         return FMLPaths.CONFIGDIR.get();
@@ -26,5 +31,10 @@ public class ForgePlatformHelper implements PlatformHelper {
     @Override
     public boolean isModLoadedAtLaunch(String modId) {
         return LoadingModList.get().getModFileById(modId) != null;
+    }
+
+    @Override
+    public void registerKeyBinding(KeyBinding keyBinding) {
+        KEYBINDINGS.add(keyBinding);
     }
 }
--- forge/src/main/java/com/stalemated/lib/forge/SLibForgeClient.java
+++ forge/src/main/java/com/stalemated/lib/forge/SLibForgeClient.java
@@ -1,12 +1,23 @@
 package com.stalemated.lib.forge;
 
+import net.minecraft.client.option.KeyBinding;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import com.stalemated.lib.SLibClient;
 
 @Mod(SLibClient.MOD_ID)
+@SuppressWarnings("removal")
 public final class SLibForgeClient {
     public SLibForgeClient() {
         SLibClient.init();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onRegisterKeyMappings);
+    }
+
+    private void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
+        for (KeyBinding kb : ForgePlatformHelper.KEYBINDINGS) {
+            event.register(kb);
+        }
     }
 }
