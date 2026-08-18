ec1f414929c9a000c04e8da4765498cc19325361
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-08-18 14:53:14 -0600

    Feat: add 3 different easing functions (lerp, ease out cubic, ease out expo)

--- common/src/main/java/com/stalemated/lib/util/math/MathUtils.java
+++ common/src/main/java/com/stalemated/lib/util/math/MathUtils.java
@@ -5,4 +5,16 @@ public class MathUtils {
         if (value < min) return min;
         return Math.min(value, max);
     }
+
+    public static double lerp(double start, double end, double t) {
+        return start + (end - start) * t;
+    }
+
+    public static double easeOutCubic(double t) {
+        return 1.0 - Math.pow(1.0 - t, 3.0);
+    }
+
+    public static double easeOutExpo(double t) {
+        return t == 1.0 ? 1.0 : 1.0 - Math.pow(2.0, -10.0 * t);
+    }
 }
