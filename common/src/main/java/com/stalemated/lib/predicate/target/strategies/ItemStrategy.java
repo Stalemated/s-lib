ebe9b9ceb4a0ba3ea45a2d0e2e0ce488521b1200
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-08-26 22:36:32 -0600

    Fix: fixed race condition in Forge making modded items not match due to early registration

--- common/src/main/java/com/stalemated/lib/predicate/target/strategies/ItemStrategy.java
+++ common/src/main/java/com/stalemated/lib/predicate/target/strategies/ItemStrategy.java
@@ -1,18 +1,19 @@
 package com.stalemated.lib.predicate.target.strategies;
 
 import com.stalemated.lib.predicate.target.TargetMatcher;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
 public class ItemStrategy implements TargetMatcher {
-    private final Item item;
-    public ItemStrategy(String itemId) { this.item = Registries.ITEM.get(new Identifier(itemId)); }
+    private final Identifier itemId;
+
+    public ItemStrategy(String itemId) {
+        this.itemId = new Identifier(itemId);
+    }
 
     @Override
     public boolean matches(ItemStack stack) {
-        if (item == null) return false;
-        return stack.isOf(item);
+        return Registries.ITEM.getId(stack.getItem()).equals(this.itemId);
     }
 }
